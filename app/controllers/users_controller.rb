3f7f7d4c5f731e72923b3dfabaa4f3a0bd9a1373
karvel-code <velgithinji@gmail.com>
2021-06-04 11:09:12 +0300

    Remove missing frozen literal comments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class UsersController < ApplicationController
   def index
     @users = User.all
@@ -9,6 +7,5 @@ class UsersController < ApplicationController
     @user = User.find_by(id: params[:id])
     @upcoming_events = current_user.attended_events.upcoming_events
     @past_events = current_user.attended_events.past_events
-    # @created_events = current_user.created_events
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 module ApplicationHelper
   def user_options
     if user_signed_in?
