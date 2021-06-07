a9d6eadd495bbaf1133939d1d02f4083b1831050
karvel-code <velgithinji@gmail.com>
2021-06-07 16:06:58 +0300

    Add tests for event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,7 @@
 class Event < ApplicationRecord
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :date, presence: true
   belongs_to :creator, class_name: 'User'
   has_many :invitations
   has_many :attendees, through: :invitations, source: :user
